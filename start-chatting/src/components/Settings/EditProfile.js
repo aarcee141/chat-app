8afdeddb8efc5cc16300c24eea56997160680c7f
Siddhant Gautam <siddhant2219@gmail.com>
2023-04-30 16:07:41 +0530

    Beautify Edit Profile.

--- start-chatting/src/components/Settings/EditProfile.js
+++ start-chatting/src/components/Settings/EditProfile.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import firebase from "firebase/compat/app";
 import { useNavigate } from "react-router-dom";
 import uploadProfilePic from "../../services/UploadProfilePic";
+import "./EditProfile.css";
 
 function EditProfile() {
     const auth = firebase.auth();
@@ -26,18 +27,22 @@ function EditProfile() {
     };
 
     return (
-        <div>
+        <div className="edit-profile-container">
             <h1>Edit Profile</h1>
-            <div>
+            <div className="edit-profile-input-container">
                 <label>
-                    Upload Profile Picture:
+                    <span>Upload Profile Picture:</span>
                     <input type="file" onChange={handleFileChange} />
                 </label>
             </div>
-            <button onClick={handleUpdateProfilePic}
-            >Save Changes</button>
-            <button onClick={handleGoBack}>Go Back!</button>
-
+            <div className="edit-profile-button-container">
+                <button className="save-changes-button" onClick={handleUpdateProfilePic}>
+                    Save Changes
+                </button>
+                <button className="go-back-button" onClick={handleGoBack}>
+                    Go Back
+                </button>
+            </div>
         </div>
     );
 }
